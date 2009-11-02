b6cfb252b14252d768364f4114e1e848bbf442d3
Matthew Todd <matthew.todd@gmail.com>
2009-11-02 16:57:33 +0300

    Gnarly Passenger/Rack/Sinatra incompatibility?

--- app/metal/tapsuey_server.rb
+++ app/metal/tapsuey_server.rb
@@ -11,10 +11,27 @@ class TapsueyServer
     Taps::Config.password          = Tapsuey.password
 
     require 'taps/server'
-    @app = Rack::URLMap.new('/tapsuey' => Taps::Server.new)
+    tapsuey = Rack::Builder.app do
+      use FixPassengerNonRewindableInput
+      run Taps::Server.new
+    end
+
+    @app = Rack::URLMap.new('/tapsuey' => tapsuey)
   end
 
   def call(env)
     @app.call(env)
+  end
+
+  private
+
+  # NoMethodError at /tapsuey/
+  # undefined method `rewind' for #<UNIXSocket:0x9636fec>
+  # file: request.rb location: POST line: 142
+  class FixPassengerNonRewindableInput < Struct.new(:app)
+    def call(env)
+      env['rack.input'] = Rack::RewindableInput.new(env['rack.input'])
+      app.call(env)
+    end
   end
 end
