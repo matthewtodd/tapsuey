2e8a7191bcffc2379b20750adddc70ff64a07a61
Matthew Todd <matthew.todd@gmail.com>
2009-11-05 11:57:32 +0300

    cap tapsuey:db:pull

--- recipes/tapsuey.rb
+++ recipes/tapsuey.rb
@@ -4,6 +4,18 @@
 depend :remote, :gem, 'activerecord', '= 2.2.2'
 
 namespace :tapsuey do
+  namespace :db do
+    desc 'Pull production data into the latest release.'
+    task :pull do
+      rake      = fetch(:rake, 'rake')
+      rails_env = fetch(:rails_env, 'production')
+
+      unless rails_env == 'production'
+        run "cd #{latest_release}; #{rake} RAILS_ENV=#{rails_env} db:pull"
+      end
+    end
+  end
+
   desc 'Download production tapsuey password to config/tapsuey.txt.'
   task :download_password do
     download "#{shared_path}/config/tapsuey.txt", 'config/tapsuey.txt'
