fd286f11cd8ea128efef064033a2b89269343948
Matthew Todd <matthew.todd@gmail.com>
2009-11-05 16:44:06 +0300

    Environment variables are cooler than text files.

--- generators/tapsuey/tapsuey_generator.rb
+++ generators/tapsuey/tapsuey_generator.rb
@@ -2,7 +2,6 @@ class TapsueyGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.template 'initializer.rb', 'config/initializers/tapsuey.rb'
-      m.template 'password.txt',   'config/tapsuey.txt'
     end
   end
 end
--- recipes/tapsuey.rb
+++ recipes/tapsuey.rb
@@ -15,27 +15,4 @@ namespace :tapsuey do
       end
     end
   end
-
-  desc 'Download production tapsuey password to config/tapsuey.txt.'
-  task :download_password do
-    download "#{shared_path}/config/tapsuey.txt", 'config/tapsuey.txt'
-  end
-
-  desc 'Upload config/tapsuey.txt to production shared_path.'
-  task :upload_password do
-    # TODO would it make more sense to add 'config' into shared_children?
-    run "#{try_sudo} mkdir -p #{shared_path}/config && #{try_sudo} chmod g+w #{shared_path}/config"
-    upload 'config/tapsuey.txt', "#{shared_path}/config/tapsuey.txt", :mode => 0600
-  end
-
-  desc 'Symlink config/tapsuey.txt into the latest release.'
-  task :symlink_password do
-    run <<-CMD
-      rm -rf #{latest_release}/config/tapsuey.txt &&
-      ln -s #{shared_path}/config/tapsuey.txt #{latest_release}/config/tapsuey.txt
-    CMD
-  end
 end
-
-after 'deploy:setup',           'tapsuey:upload_password'
-after 'deploy:finalize_update', 'tapsuey:symlink_password'
