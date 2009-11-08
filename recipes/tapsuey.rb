0cc6bb9bc7d69c1751c271978efe215f57f7c6c4
Matthew Todd <matthew.todd@gmail.com>
2009-11-08 15:08:20 +0300

    Remote dependency on ar-2.2.2 no longer exists.

--- recipes/tapsuey.rb
+++ recipes/tapsuey.rb
@@ -1,8 +1,3 @@
-# Our vendored version of taps has bin/schema with an explicit gem dependency
-# on activerecord-2.2.2. We don't specify this as a dependency in
-# rails/init.rb because it would wreak havoc with all that is holy.
-depend :remote, :gem, 'activerecord', '= 2.2.2'
-
 namespace :tapsuey do
   namespace :db do
     desc 'Pull production data into the latest release.'
